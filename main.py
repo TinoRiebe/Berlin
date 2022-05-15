c6c7a408e4c04db72d550ddf1beede155a3856dd
TinoRiebe <tino.riebe@gmx.net>
2022-05-15 17:40:06 +0200

    v2

--- main.py
+++ main.py
@@ -19,6 +19,10 @@ class MainWindow(QMainWindow):
         self.pb_loadmsrfiles.clicked.connect(self.load_msr_files)
         self.pb_exit.clicked.connect(self.close)
         self.pb_exit.setStyleSheet('color: red')
+        self.pb_test.clicked.connect(self.file_list)
+        self.csv_list = []
+        self.par_list = []
+        self.mod_list = []
 
     def load_msr_files(self):
         for i in range(11):
@@ -26,8 +30,23 @@ class MainWindow(QMainWindow):
             QApplication.processEvents()
             sleep(0.5)
 
-    def filelist(self):
-        pass
+    def file_list(self):
+        while not os.path.isdir(self.filepath):
+            self.filepath = QFileDialog.getExistingDirectory(self, 'Select Folder')
+        self.files = os.listdir(self.filepath)
+        self.csv_list = []
+        self.par_list = []
+        self.mod_list = []
+
+        for file in self.files:
+            if file[0:3] == 'msr':
+                if file[-3:] == 'txt':
+                    self.csv_list.append(file)
+                elif file[-3:] == 'par':
+                    self.par_list.append(file)
+            elif file[-5:] == '.hdf5':
+                self.mod_list.append(file)
+        print(self.csv_list)
 
 
 if __name__ == '__main__':
